78352fee8e133de09504551f9f839e9974ea2a78
park990 <rhkgkrwk2008@gmail.com>
2025-12-08 21:28:28 +0900

    design ..ing

--- front/lib/screen/wazzup_screen/wazzup_main.dart
+++ front/lib/screen/wazzup_screen/wazzup_main.dart
@@ -5,11 +5,83 @@ class WazzupMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('WAZZUP 메인 화면  여기는 게시판들을 위한 공간임.'),
+      appBar: AppBar(
+        title: Center(
+          child: Text('WAZZUP 게시판')
+        ),backgroundColor: Colors.grey,
+      ),
+
+      body: Column(
+        children: [
+          _BestBbs(),
+          _BbsList(),
+        ],
+      ),
+    );
+  }
+}
+
+class _BestBbs extends StatelessWidget {
+  const _BestBbs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.amber,
+          width: double.infinity,
+          child: Text("Best"),
         ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.green,
+              child: Text('긂ㅇㄴㄻㅇㄻㅇㄻㄹㄴㅇ'),
+            ),
+            Container(
+              color: Colors.green,
+              child: Text('긂ㅇㄴㄻㅇㄻㅇㄻㄹㄴㅇ'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _BbsList extends StatelessWidget {
+  const _BbsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> BbsTitle = ['자유게시판', '인연찾기', '운동게시판'];
+    return Expanded(
+      child: ListView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        itemCount: BbsTitle.length,
+        itemBuilder: (context, index) {
+          final title = BbsTitle[index];
+          return Column(
+            children: [
+              SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                child: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
