7d7033ebec8443d677356987887e05112bcad64d
park990 <rhkgkrwk2008@gmail.com>
2025-12-15 16:25:01 +0900

    font

--- front/lib/screen/home_screen.dart
+++ front/lib/screen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:front/screen/bottom_nav_bar.dart';
-import 'package:front/screen/upper_app_bar.dart';
+import 'package:front/screen/nav_bar_screen/bottom_nav_bar.dart';
+import 'package:front/screen/nav_bar_screen/upper_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
