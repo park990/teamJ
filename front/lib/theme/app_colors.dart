d28357a7d77af14424774452b88e7bdba6bccd76
gyeol <gksruf2233@gmail.com>
2025-12-16 15:53:06 +0900

    style: 주석 추가

--- front/lib/theme/app_colors.dart
+++ front/lib/theme/app_colors.dart
@@ -15,25 +15,25 @@ import 'package:google_fonts/google_fonts.dart';
   final TextStyle wazzupFont = GoogleFonts.ibmPlexSans(
 );
 
-// 타이틀/헤더
+// 타이틀/헤더(앱 최상단)
 final TextStyle headerFont = GoogleFonts.ibmPlexSans(
   fontSize: 28,
   fontWeight: FontWeight.w700,
 );
 
-// 섹션 제목
+// 섹션 제목(페이지 내 특정 컨텐츠 블럭)
 final TextStyle subTitleFont = GoogleFonts.ibmPlexSans(
   fontSize: 18,
   fontWeight: FontWeight.w600,
 );
 
-// 본문
+// 본문(카드 내부)
 final TextStyle bodyFont = GoogleFonts.ibmPlexSans(
   fontSize: 14,
   fontWeight: FontWeight.w400,
 );
 
-// 보조 텍스트
+// 보조 텍스트(부가 정보 작성할경우 ex) "호스트 외 15명 참여")
 final TextStyle labelFont = GoogleFonts.ibmPlexSans(
   fontSize: 12,
 );
