75668dd6d75762a2f569e94b3ba18f8675c0e09b
gyeol <gksruf2233@gmail.com>
2025-12-16 15:41:26 +0900

    style: 텍스트폰트 정의

--- front/lib/theme/app_colors.dart
+++ front/lib/theme/app_colors.dart
@@ -14,3 +14,26 @@ import 'package:google_fonts/google_fonts.dart';
   // 기본 글 씨 테마 알아서 크기 조정하고 굵기 조정하셈
   final TextStyle wazzupFont = GoogleFonts.ibmPlexSans(
 );
+
+// 타이틀/헤더
+final TextStyle headerFont = GoogleFonts.ibmPlexSans(
+  fontSize: 28,
+  fontWeight: FontWeight.w700,
+);
+
+// 섹션 제목
+final TextStyle subTitleFont = GoogleFonts.ibmPlexSans(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+// 본문
+final TextStyle bodyFont = GoogleFonts.ibmPlexSans(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+
+// 보조 텍스트
+final TextStyle labelFont = GoogleFonts.ibmPlexSans(
+  fontSize: 12,
+);
