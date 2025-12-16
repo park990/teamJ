ea2cf5f5941dc740834c215237309091557ec750
park990 <rhkgkrwk2008@gmail.com>
2025-12-16 12:49:50 +0900

    Icons

--- front/lib/const/tabs.dart
+++ front/lib/const/tabs.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:front/screen/friend_screen/friend_main.dart';
 import 'package:front/screen/gathering_screen/gathering_main.dart';
 import 'package:front/screen/myPage_screen/myPage_main.dart';
 import 'package:front/screen/random_chat_screen/random_chat_main.dart';
 import 'package:front/screen/wazzup_screen/wazzup_main.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class TabInfo{
   final IconData icon;
@@ -19,31 +21,31 @@ class TabInfo{
 
 final TABS =[
   TabInfo(
-    icon: Icons.location_searching_outlined,
-    label: '모임소식',
+    icon: Icons.home_outlined,
+    label: 'home',
     screen: GatheringMain(),
   ),
   TabInfo(
-    icon: Icons.question_answer_outlined,
-    label: '랜덤채팅',
+    icon: CupertinoIcons.chat_bubble_2,
+    label: 'wChat',
     screen: RandomChatMain(),
 
   ),
   TabInfo(
-    icon: Icons.home_max_outlined,
+    icon: LucideIcons.zap,
     label: 'WAZZUP',
     screen: WazzupMain(),
 
   ),
   TabInfo(
-    icon: Icons.group_add_outlined,
-    label: '친구',
+    icon: CupertinoIcons.person_2,
+    label: 'friends',
     screen: FriendMain(),
 
   ),
   TabInfo(
-    icon: Icons.person_outline_outlined,
-    label: '마이',
+    icon: CupertinoIcons.person_circle,
+    label: 'profile',
     screen:MypageMain() ,
 
   ),
