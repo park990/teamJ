cca8b8c2f28bf1aee13fd0d85ec1474d8edd0f55
sjs <sjhw0229@naver.com>
2025-12-17 16:47:05 +0900

    12

---
+++ front/lib/provider/random_chat_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front/repository/random_match_repository.dart';
+import 'package:front/screen/random_chat_screen/controller/random_chat_controller.dart';
+
+/// 1. Repository Provider
+final randomMatchRepositoryProvider =
+    Provider<RandomMatchRepository>((ref) {
+  return RandomMatchRepository();
+});
+
+/// 2. Controller Provider
+final randomChatControllerProvider =
+    ChangeNotifierProvider<RandomChatController>((ref) {
+  final repo = ref.read(randomMatchRepositoryProvider);
+  return RandomChatController(matchRepository: repo);
+});
--- front/lib/repository/random_match_repository.dart
+++ front/lib/repository/random_match_repository.dart
@@ -7,21 +7,23 @@ class RandomMatchRepository {
   final String baseUrl = dotenv.env['API_URL']!;
   final andUrl = "http://10.0.2.2:8080";
 
+  final Map<String, String> _headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+
   Future<RandomMatchDto> enterQueue({
     required String genderOption,
   }) async {
     final response = await http.post(
       Uri.parse('$andUrl/api/random-match/enter'),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
+      headers: _headers,
       body: jsonEncode({
         'genderOption': genderOption,
       }),
     );
 
     if (response.statusCode != 200) {
-      throw Exception('랜덤 매칭 실패');
+      throw Exception('랜덤 매칭 실패 ${response.statusCode}');
     }
 
     final json = jsonDecode(utf8.decode(response.bodyBytes));
@@ -29,11 +31,13 @@ class RandomMatchRepository {
   }
 
   Future<void> cancelQueue() async {
-    await http.post(
+    final response = await http.post(
       Uri.parse('$andUrl/api/random-match/cancel'),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
+      headers: _headers,
     );
+
+    if (response.statusCode != 200) {
+      throw Exception('랜덤 매칭 취소 실패 ${response.statusCode}');
+    }
   }
 }
