42053573c5c5e876d9bad85815af075dff0b300c
gyeol <gksruf2233@gmail.com>
2025-12-17 18:29:00 +0900

    style: 모임페이지 초안

--- front/lib/screen/gathering_screen/gathering_main.dart
+++ front/lib/screen/gathering_screen/gathering_main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:front/screen/gathering_screen/model/gathering_meeting.dart';
+import 'package:front/screen/gathering_screen/widgets/ad_banner.dart';
+import 'package:front/screen/gathering_screen/widgets/gathering_card_item.dart';
 import 'package:front/theme/app_colors.dart';
 
 class GatheringMain extends StatefulWidget {
-  const GatheringMain({super.key});
+
+  GatheringMain({super.key});
 
   @override
   State<GatheringMain> createState() => _GatheringMainState();
@@ -11,7 +15,29 @@ class GatheringMain extends StatefulWidget {
 class _GatheringMainState extends State<GatheringMain> 
   with TickerProviderStateMixin {
 
-    bool isSearching = false;
+    List<GatheringMeeting> _gatheringMeetings = [
+    //더미데이터
+    GatheringMeeting(
+      cardTitle: '96년생 모여라~!',
+      cardBody: '동갑 친구들끼리 허심탄회하게 얘기해봐용',
+      cardImageUrl: 'asset/img/image.png',
+      cardParticipantsCount: 5,
+    ),
+    GatheringMeeting(
+      cardTitle: '여미새 남미새 다 모여라~~',
+      cardBody: '매력발산 시작~',
+      cardImageUrl: 'asset/img/image.png',
+      cardParticipantsCount: 10,
+    ),
+    GatheringMeeting(
+      cardTitle: 'TeamJ',
+      cardBody: '코딩 쌉고수 모임',
+      cardImageUrl: 'asset/img/image.png',
+      cardParticipantsCount: 3,
+    ),
+  ];
+
+    //bool isSearching = false;
 
     @override
     Widget build(BuildContext context) {
@@ -19,114 +45,31 @@ class _GatheringMainState extends State<GatheringMain>
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // 광고 배너 영역
-            Container(
-              height: 70,
-              color: Colors.grey[400],
-              alignment: Alignment.center,
-              child: Text('여기에 광고 배너(이미지)'),
+            // 고정: 광고 배너 영역
+            AdBanner(),
+            // Container(
+            //   height: 70,
+            //   color: Colors.grey[400],
+            //   alignment: Alignment.center,
+            //   child: Text('여기에 광고 배너(이미지)'),
+            // ),
+            // 고정 : 섹션 제목
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text('🔥이번 주 핫한 모임', style: sectionTitleFont),
             ),
-            Container(
-              child: Padding(
-                //padding: const EdgeInsets.all(18.0),
-                padding: const EdgeInsets.only(left: 3),
-                child: Text('🔥이번 주 핫한 모임', style: subTitleFont),
+
+            // ListView가 들어갈 곳
+            Expanded(
+                child: ListView.separated(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  itemCount: _gatheringMeetings.length,
+                  separatorBuilder: (context, index) => const SizedBox(height: 16),
+                  itemBuilder: (context, index) {
+                    return GatheringCardItem(meeting: _gatheringMeetings[index]);
+                  }
+                ),
               ),
-            ),
-            // 모임 카드 1
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: 180.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                    ),
-                    child: Image.asset(
-                      'asset/img/image.png',
-                      fit: BoxFit.cover,
-                      // 에러 디버깅용
-                      errorBuilder: (context, error, StackTrace) {
-                        return Container(
-                          //color: Colors.grey,
-                          child: Icon(Icons.error),
-                        );
-                      }
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    child: Column(
-                      children: [
-                        Text(
-                          '96년생 모여라~!', style: subTitleFont,
-                          maxLines: 4,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    ),
-                    padding: EdgeInsets.all(12),
-                  ),
-                ),
-                SizedBox(
-                  width: 80.0,
-                  height: 4.0,
-                ),
-              ],
-            ),
-            //모임 카드 2
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: 180.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                    ),
-                    child: Image.asset(
-                      'asset/img/image.png',
-                      fit: BoxFit.cover,
-                      // 에러 디버깅용
-                      errorBuilder: (context, error, StackTrace) {
-                        return Container(
-                          //color: Colors.grey,
-                          child: Icon(Icons.error),
-                        );
-                      }
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    child: Column(
-                      children: [
-                        Text(
-                          '너 빼고 다 참가중ㅋ', style: subTitleFont,
-                          maxLines: 4,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    ),
-                    padding: EdgeInsets.all(12),
-                  ),
-                ),
-                SizedBox(
-                  width: 60.0,
-                  height: 4.0,
-                ),
-              ],
-            ),
           ],
         ),
       );
