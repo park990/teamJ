e746c830ed4a204362b5d2c2c1a4af0d23f709f4
gyeol <gksruf2233@gmail.com>
2026-01-07 20:19:00 +0900

    refactor: ApiResponse 규격 통일 및 비즈니스 로직 분리

--- backend/src/main/java/com/teamj/controller/gathering_control/GatheringController.java
+++ backend/src/main/java/com/teamj/controller/gathering_control/GatheringController.java
@@ -2,9 +2,15 @@ package com.teamj.controller.gathering_control;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.teamj.dto.gathering_dto.GatheringHotListDto;
+import com.teamj.dto.response.ApiResponse;
 import com.teamj.service.gathering_service.GatheringService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,12 +24,15 @@ public class GatheringController {
     // 핫한 모임 호출
     @GetMapping("/hotList")
     public ResponseEntity<?> getHotList() {
-        return gatheringService.getHotGatheringList();
+        List<GatheringHotListDto> list = gatheringService.getHotGatheringHotList();
+        //return gatheringService.getHotGatheringList();
+        return ResponseEntity.ok(ApiResponse.success(list, "핫한 모임 목록 조회 성공"));
     }
     // 새로운 모임 목록
     @GetMapping("/newList")
     public ResponseEntity<?> getNewList() {
-        return gatheringService.getNewGatheringList();
+        List<GatheringHotListDto> list = gatheringService.getNewGatheringList();
+        return ResponseEntity.ok(ApiResponse.success(list, "새로운 모임 목록 조회 성공"));
     }
     
 }
