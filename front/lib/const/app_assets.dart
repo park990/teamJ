9e4b1d21fb6abdd7331852acb75144414ce5b8b3
gyeol <gksruf2233@gmail.com>
2026-01-07 14:34:04 +0900

    refactor: AppAssets로 기본이미지 경로 관리

---
+++ front/lib/const/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  // AppAssets() 생성 못하기 막기
+  AppAssets._();
+  // static const로 메모리에 올리기
+  static const String defaultProfile = 'asset/img/image.png';
+  // 필요에 따라 다른 이미지도 추가하면됨
+}
