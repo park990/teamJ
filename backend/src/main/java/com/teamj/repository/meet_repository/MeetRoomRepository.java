122bec47ef39487247be5b3da5915ff7cfd4b807
gyeol <gksruf2233@gmail.com>
2026-01-09 17:48:42 +0900

    refactor: 핫한 모임 불러오기 시 랜덤채팅방 제외

--- backend/src/main/java/com/teamj/repository/meet_repository/MeetRoomRepository.java
+++ backend/src/main/java/com/teamj/repository/meet_repository/MeetRoomRepository.java
@@ -8,7 +8,9 @@ import com.teamj.entity.meet_entity.MeetRoom;
 
 public interface MeetRoomRepository extends JpaRepository<MeetRoom, Long>{
     // 핫한 모임(5개)
-    List<MeetRoom> findTop5ByOrderByCreatedAtDesc();
+    //List<MeetRoom> findTop5ByOrderByCreatedAtDesc();
+    // 핫한 모임(RANDOM_1ON1 모임 제외)
+    List<MeetRoom> findTop5ByRoomTypeNotOrderByCreatedAtDesc(String roomType);
     // 최신 모임(전부)
     List<MeetRoom> findAllByOrderByCreatedAtDesc();
 }
