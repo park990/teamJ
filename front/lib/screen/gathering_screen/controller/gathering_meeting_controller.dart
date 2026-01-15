7a2b2c0478eafb0a0453d457e84a2b409568edbb
gyeol <gksruf2233@gmail.com>
2026-01-15 20:54:33 +0900

    feat: 모임 컨트롤러 로직

--- front/lib/screen/gathering_screen/controller/gathering_meeting_controller.dart
+++ front/lib/screen/gathering_screen/controller/gathering_meeting_controller.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:front/data/repository/gathering_repository.dart';
+import 'package:front/dto/gatherings/gathering_detail_dto.dart';
 
-// class GatheringMeetingController extends ChangeNotifier {
-//   final GatheringMeetingRepository gatheringMeetingRepository;
-// }
+class GatheringMeetingController extends ChangeNotifier{
+  
+  final GatheringRepository repository;
+  // nullable
+  GatheringDetailDto? detailDto;
+  
+  bool isLoading = false;
+  
+  //생성자
+  GatheringMeetingController({
+    required this.repository,
+  });
+}
