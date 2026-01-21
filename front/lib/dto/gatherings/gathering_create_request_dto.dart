4785a56a9d285cfb1a7fd3d7054a62b3fc75ecc9
gyeol <gksruf2233@gmail.com>
2026-01-21 20:45:07 +0900

    feat: 모임 요청용 dto 추가

---
+++ front/lib/dto/gatherings/gathering_create_request_dto.dart
@@ -0,0 +1,43 @@
+class GatheringCreateRequestDto {
+  final String roomName;
+  final String roomType;
+  final String roomDesc;
+  //final String? roomImg; // 이미지 업로드는 백에서 multipart로 처리
+  final DateTime meetDate;
+  final String meetPlace;
+  final int max;
+
+  const GatheringCreateRequestDto({
+    required this.roomName,
+    required this.roomType,
+    required this.roomDesc,
+    //this.roomImg,
+    required this.meetDate,
+    required this.meetPlace,
+    required this.max
+  });
+
+  factory GatheringCreateRequestDto.fromJson(Map<String, dynamic> json) {
+    return GatheringCreateRequestDto(
+      roomName: json['roomName'] as String,
+      roomType: json['roomType'] as String,
+      roomDesc: json['roomDesc'] as String,
+      //roomImg: json['roomImg'] as String?,
+      meetDate: DateTime.parse(json['meetDate'] as String),
+      meetPlace: json['meetPlace'] as String,
+      max: json['max'] as int,
+    );
+  }
+
+  // Dart 객체 → JSON으로 변환 (백엔드로 보낼 때)
+  Map<String, dynamic> toJson() {
+    return {
+      'roomName': roomName,
+      'roomType': roomType,
+      'roomDesc': roomDesc,
+      'meetDate': meetDate.toIso8601String(),  // DateTime → "2025-01-21T14:00:00.000Z" 형태
+      'meetPlace': meetPlace,
+      'max': max,
+    };
+  }
+}
