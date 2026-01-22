5dbeaa982d8549adc4611a3392fcb4b4499e09c2
gyeol <gksruf2233@gmail.com>
2026-01-22 11:32:13 +0900

    feat: Notifier를 위한 State 클래스 추가

--- front/lib/screen/gathering_screen/providers/gathering_create_state.dart
+++ front/lib/screen/gathering_screen/providers/gathering_create_state.dart
@@ -1,3 +1,34 @@
+import 'package:front/dto/gatherings/gathering_action_response_dto.dart';
+
 class GatheringCreateState {
-  
+  final bool? isLoading;
+  final GatheringActionResponseDto? result;
+  final String? errorMessage;
+
+  const GatheringCreateState({
+    required this.isLoading,
+    this.result,
+    this.errorMessage,
+  });
+
+  factory GatheringCreateState.initial() {
+    return const GatheringCreateState(
+      isLoading: false,
+      result: null,
+      errorMessage: null,
+    );
+  }
+
+  GatheringCreateState copyWith({
+    bool? isLoading,
+    GatheringActionResponseDto? result,
+    String? errorMessage,
+  }) {
+    return GatheringCreateState(
+      isLoading: isLoading ?? this.isLoading,
+      result: result ?? this.result,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
 }
